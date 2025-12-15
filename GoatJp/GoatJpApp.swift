e3ff498ba70b90c162812432bf1269ceeedc4603
tom <tommy.yohanes@icloud.com>
2025-12-15 21:49:29 +0700

    Initial Commit

---
+++ GoatJp/GoatJpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoatJpApp.swift
+//  GoatJp
+//
+//  Created by tomtomtom on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GoatJpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoatJpTests/GoatJpTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoatJpTests.swift
+//  GoatJpTests
+//
+//  Created by tomtomtom on 12/15/25.
+//
+
+import Testing
+@testable import GoatJp
+
+struct GoatJpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
